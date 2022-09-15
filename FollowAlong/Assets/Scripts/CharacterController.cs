12426ef074a53ba4a2ebd56c2247b320fb06e800
SmizzySmiz <thecolorhannah@hotmail.co.uk>
2022-09-15 15:39:24 +0200

    Shooting and Turns

---
+++ FollowAlong/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody characterBody;
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private PlayerTurn playerTurn;
+
+    void Update()
+    {
+        if(playerTurn.IsPlayerTurn())
+        {
+            if (Input.GetAxis("Horizontal") != 0)
+            {
+                transform.Translate(transform.right * speed * Time.deltaTime * Input.GetAxis("Horizontal"));
+            }
+
+            if (Input.GetAxis("Vertical") != 0)
+            {
+                transform.Translate(transform.forward * speed * Time.deltaTime * Input.GetAxis("Vertical"));
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space) && IsTouchingFloor())
+            {
+                Jump();
+            }
+        }
+    }
+
+    private void Jump()
+    {
+        characterBody.AddForce(Vector3.up * 200f);
+    }
+
+    private bool IsTouchingFloor()
+    {
+        RaycastHit hit;
+        // Parameters:
+        // - The center from where we shoot
+        // - Radius of the sphere
+        // - Direction of the sphere
+        // - hit parameter
+        // - Distance the sphere
+        bool result = Physics.SphereCast(transform.position, 0.15f, -transform.up, out hit, 1f);
+        return result;
+    }
+   
+}
---
+++ FollowAlong/Assets/Scripts/Projectile.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Rigidbody projectileBody;
+    [SerializeField] private GameObject damageIndicatorPrefab;
+    private bool isActive;
+
+    public void Initialize()
+    {
+        isActive = true;
+        // -------- This method is for projectiles that have a parabole. ----------
+        // We add a force only once, not every frame
+        // Make sure to have "useGravity" toggled on in the rigid body
+        projectileBody.AddForce(transform.forward * 700f + transform.up * 100f);
+    }
+
+    void Update()
+    {
+        if (isActive)
+        {
+            transform.Translate(transform.forward * speed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+          //GameObject collisionObject = collision.gameObject;
+          //DestructionFree destruction = collisionObject.GetComponent<DestructionFree>();
+          //if (GetComponent<DestructionFree>())
+        
+          {
+            //Destroy(collisionObject);
+            GameObject damageIndicator = Instantiate(damageIndicatorPrefab);
+            damageIndicator.transform.position = collision.GetContact(0).point;
+            
+            Destroy(gameObject);
+                        
+          }
+          
+    }
+}
